3e35b19382452ac1ba3848125530654db23e6f64
Robert Hutchinson <robert.hutchinson@gmail.com>
2015-03-24 16:57:44 -0400

    First pass at Omniauth and User creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,24 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, and :timeoutable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, 
+         :omniauthable, omniauth_providers: [:github]
+
+  serialize :github_data, JSON
+
+  def self.from_omniauth data
+    github_id = data.uid
+    if user = User.find_by(github_id: github_id)
+      user
+    else
+      where(github_id: github_id).create! do |u|
+        u.email = data["info"]["email"]
+        u.password = SecureRandom.hex 64
+        u.github_id = github_id
+        u.auth_data = data
+      end
+    end
+  end
+
 end

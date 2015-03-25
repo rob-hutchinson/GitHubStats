d4bd45c4d2adda3caf05ec13b8e0a28901110466
Robert Hutchinson <robert.hutchinson@gmail.com>
2015-03-24 21:49:37 -0400

    Finalized Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,20 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, and :timeoutable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, 
          :omniauthable, omniauth_providers: [:github]
 
-  serialize :github_data, JSON
+  serialize :auth_data, JSON
 
   def self.from_omniauth data
     github_id = data.uid
-    if user = User.find_by(github_id: github_id)
-      user
-    else
-      where(github_id: github_id).create! do |u|
+    user = User.where(github_id: github_id).first_or_create! do |u|
         u.email = data["info"]["email"]
         u.password = SecureRandom.hex 64
         u.github_id = github_id
+        u.nickname = data["info"]["nickname"]
         u.auth_data = data
-      end
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324184316) do
+ActiveRecord::Schema.define(version: 20150324210815) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150324184316) do
     t.datetime "updated_at"
     t.string   "github_id"
     t.text     "auth_data"
+    t.string   "nickname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
